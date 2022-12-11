4f8debb56a06b36ede9d1b5bd4e2550c6dde5301
itsdhanoob <ben.birmole@gmail.com>
2022-12-11 12:51:50 +0100

    edits

--- cc/c.py
+++ cc/c.py
@@ -27,45 +27,19 @@ objp = objp * size_of_chessboard_squares_mm
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-num_img=0
 images = glob.glob('*.jpg')
 
 for image in images:
-	img = cv.imread(image)
-	gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-		 
-		# Find the chess board corners
-	ret, corners = cv.findChessboardCorners(img, chessboardSize, flags=cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_FAST_CHECK+
-																				  cv.CALIB_CB_NORMALIZE_IMAGE+cv.CALIB_CB_FILTER_QUADS)
-		# If found, add object points, image points (after refining them)
-	if ret == True:
-		objpoints.append(objp)
-		corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
-		imgpoints.append(corners)
-
-		# Draw and display the corners
-		cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
-		num_img+=1
-		cv.imshow('img', img)
-		cv.waitKey(0)
-	else:
-		print("somethings wrong")
-
     img = cv.imread(image)
-    # norm= np.zeros((800,800))
-    # norm_img= cv.normalize(img, norm,0,255,cv.NORM_MINMAX)
-    # img = cv.resize(img, (640, 480))
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    cv.imshow("img",gray)
-    cv.waitKey(0)
 
     # Find the chess board corners
     ret, corners = cv.findChessboardCorners(img, chessboardSize, flags=cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_FAST_CHECK+
-                                                                    cv.CALIB_CB_NORMALIZE_IMAGE+cv.CALIB_CB_FILTER_QUADS)
+                                                                        cv.CALIB_CB_NORMALIZE_IMAGE+cv.CALIB_CB_FILTER_QUADS)
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray, corners, (14,14), (-1,-1), criteria)
+        corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners)
 
         # Draw and display the corners
@@ -75,12 +49,32 @@ for image in images:
     else:
         print("somethings wrong")
 
+# Find the chess board corners
+ret, corners = cv.findChessboardCorners(img, chessboardSize, flags=cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_FAST_CHECK+
+                                                    cv.CALIB_CB_NORMALIZE_IMAGE+cv.CALIB_CB_FILTER_QUADS)
+# If found, add object points, image points (after refining them)
+if ret == True:
+
+    objpoints.append(objp)
+    corners2 = cv.cornerSubPix(gray, corners, (14,14), (-1,-1), criteria)
+    imgpoints.append(corners)
+
+    # Draw and display the corners
+    cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
+    cv.imshow('img', img)
+    cv.waitKey(0)
+
+else:
+    print("somethings wrong")
+
+
 cv.destroyAllWindows()
 print('calibrating_camera')
 
 ############# CALIBRATION #######################################################
 
 ret, cameraMatrix, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, frameSize, None, None)
+
 
 print('/n ret is:', ret)
 print('/n camera matrix is:', cameraMatrix)
