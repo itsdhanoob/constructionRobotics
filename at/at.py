13ba7a5231f1ac70601e1e8fd8708a91e59c6c1d
itsdhanoob <ben.birmole@gmail.com>
2022-12-13 13:54:38 +0000

    corrected pose estimation

--- at/at.py
+++ at/at.py
@@ -1,4 +1,5 @@
 from apriltag import apriltag
+import numpy as np
 import cv2 as cv 
 import glob 
 
@@ -13,33 +14,43 @@ for image in images:
 
 # pose estimation
 
-cam_matrix = [[566.17993276,   0.,        318.86306433],
+cam_mat = [[566.17993276,   0.,        318.86306433],
 				[  0.,         567.08593332, 212.10183606],
 				[  0. ,          0.,           1.,        ]]
 				
-dist_matrix = ([[-1.26091809e+01, -7.63465264e+01, -1.56168174e-02,  5.30016919e-03,
-					[1.03834540e+03, [-1.28762768e+01, -7.05881320e+01,1.00726917e+03,
-					[0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  0.00000000e+00,
-						[0.00000000e+00,  0.00000000e+00,]]]]]])
+				
+cam_matrix = np.asarray(cam_mat)
+				
+				
+dist_mat = [[-1.26091809e+01, -7.63465264e+01, -1.56168174e-02,  5.30016919e-03,
+   1.03834540e+03, -1.28762768e+01, -7.05881320e+01,  1.00726917e+03,
+   0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  0.00000000e+00,
+   0.00000000e+00,  0.00000000e+00]]
+
+				
+dist_matrix= np.asarray(dist_mat)				
 				
 # 2d points from apriltag ccw		
 			
-image_points = ([[221.46931458, 320.22940063],
+image_point = ([[221.46931458, 320.22940063],
 				[416.48123169, 327.65234375],
 				[417.38134766, 143.02896118],
 				[233.71627808, 127.21188354]])
 				
+image_points= np.asarray(image_point)
 				
 # 3d real world points alwyas from center of tag
 
-object_points = [[-0.05 , -0.05 , 0],
-				[0.05, -0.05, 0],
+object_point = [[-0.05 , 0.05 , 0],
 				[0.05, 0.05, 0],
-				[-0.05, 0.05, 0]]
+				[0.05, -0.05, 0],
+				[-0.05, -0.05, 0]]
+				
+object_points= np.asarray(object_point)
 
 # cv pose estimations
 
-ret, rvecs, tvecs = cv.solvePnP(object_points, image_points, cam_matrix, dist_matrix, flags=SOLVEPNP_IPPE_SQUARE)
+ret, rvecs, tvecs = cv.solvePnP(object_points, image_points, cam_matrix, dist_matrix, flags=cv.SOLVEPNP_IPPE_SQUARE)
 
 
 print("rotation is:",rvecs)
